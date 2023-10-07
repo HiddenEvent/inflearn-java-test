26468ac28a78b1beb30fa00b733bc9a8bfb93cf5
RichardKim <hiddeneventor@gmail.com>
2023-10-08 03:32:01 +0900

    Initial commit

---
+++ src/test/java/com/whiteship/inflearnjavatest/InflearnJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whiteship.inflearnjavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
