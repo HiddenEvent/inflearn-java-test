c072a6de87e4f7ab98ae63b7fda0751d3c5309ee
RichardKim <hiddeneventor@gmail.com>
2023-10-09 23:56:25 +0900

    mock Stubbing 방식

--- src/test/java/com/whiteship/inflearnjavatest/study/StudyServiceTest.java
+++ src/test/java/com/whiteship/inflearnjavatest/study/StudyServiceTest.java
@@ -1,12 +1,19 @@
 package com.whiteship.inflearnjavatest.study;
 
+import com.whiteship.inflearnjavatest.domain.Member;
+import com.whiteship.inflearnjavatest.domain.Study;
 import com.whiteship.inflearnjavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -16,8 +23,24 @@ class StudyServiceTest {
     @Test
     void createNewStudy(@Mock MemberService memberService,
                         @Mock StudyRepository studyRepository) {
-        StudyService studyService = new StudyService(memberService, studyRepository);
-        assertNotNull(studyService);
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("a@a.com");
+        when(memberService.findById(any())).thenReturn(Optional.of(member));
+        // 검증 코드
+        Optional<Member> optionalMember = memberService.findById(1L);
+        assertEquals("a@a.com", optionalMember.get().getEmail());
+
+        doThrow(new IllegalArgumentException()).when(memberService).validate(1L);
+        assertThrows(IllegalArgumentException.class, () -> {
+            memberService.validate(1L);
+        });
+        memberService.validate(2L);
+
+//        Study study = new Study(10, "java");
+//        when(studyRepository.save(any())).thenReturn(study);
+
+
     }
 
     @Test
