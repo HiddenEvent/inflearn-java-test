b234c7f917d26be061c91e7d71fd6c2f8f581f48
RichardKim <hiddeneventor@gmail.com>
2023-10-09 16:03:22 +0900

    exception 검증

--- src/test/java/com/whiteship/inflearnjavatest/StudyTest.java
+++ src/test/java/com/whiteship/inflearnjavatest/StudyTest.java
@@ -10,17 +10,11 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기")
     void create() {
-        Study study = new Study();
 
-        assertAll(
-                () -> assertNotNull(study),
-                () -> assertEquals(StudyStatus.DRAFT, study.getStatus()
-                        , () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다."),
-                () -> assertTrue(study.getLimit() > 0
-                        , () -> "최대 참석인원은 0보다 커야한다.")
-       );
-        // () -> "스터디를 처음 만들면 상태값이 DRAFT여야 한다."
-        // 위 코드처럼 표현하는 이유는 ?=> 람다로 사용하는 이유는 실패했을 경우에만 해당 문자열 연산이 일어나기 때문인다.
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        exception.getMessage();
+        assertEquals("limit은 0보다 커야한다", exception.getMessage());
+
 
     }
 
